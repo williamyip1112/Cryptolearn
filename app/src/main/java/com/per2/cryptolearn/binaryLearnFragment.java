bc888cb542c25697b60a29502461436e416449d6
William <williamyip1112@gmail.com>
2018-12-06 09:20:55 -0800

    Worked on Learn Binary article

--- app/src/main/java/com/per2/cryptolearn/binaryLearnFragment.java
+++ app/src/main/java/com/per2/cryptolearn/binaryLearnFragment.java
@@ -9,33 +9,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class binaryLearnFragment extends android.support.v4.app.Fragment
-{
+public class binaryLearnFragment extends android.support.v4.app.Fragment {
     private TextView article;
-    private TextView video;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_binary_learn, container, false);
         wireWidgets(rootView);
-        setListeners();
         return rootView;
-    }
-    public void setListeners()
-    {
-        article.setOnClickListener((View.OnClickListener) this);
-        video.setOnClickListener((View.OnClickListener) this);
     }
 
     public void wireWidgets(View view)
     {
         article = view.findViewById(R.id.textView_main_article);
-        video = view.findViewById(R.id.textView_main_video);
     }
-
-
-
-
 }
 
